dd2bca65db92a4fb79190c64e71874ab663ef251
fcorreia18 <francisco.correia1804@gmail.com>
2023-01-16 11:35:33 +0100

    refactor: method filter from school params with slug style

--- src/modules/school/repositories/implementations/SchoolRepository.ts
+++ src/modules/school/repositories/implementations/SchoolRepository.ts
@@ -85,12 +85,31 @@ export class SchoolRepository implements ISchoolRepository {
         degrees?: Degree[];
         schools?: School[];
     } | void> {
-        console.log(province, degree, course);
+        //     const carsQuery = await this.repository
+        //     .createQueryBuilder("c")
+        //     .where("available = :available", { available: true });
+
+        //   if (brand) {
+        //     carsQuery.andWhere("brand = :brand", { brand });
+        //   }
+
+        //   if (name) {
+        //     carsQuery.andWhere("name = :name", { name });
+        //   }
+
+        //   if (category_id) {
+        //     carsQuery.andWhere("category_id = :category_id", { category_id });
+        //   }
+
+        //   const cars = await carsQuery.getMany();
+
+        //   return cars;
+        const slug = course.replace("-", " ");
 
         const degrees = await AppDataSource.manager.find(Degree, {
             where: {
                 name: degree,
-                courses: { name: course, schools: { province } },
+                courses: { name: slug, schools: { province } },
             },
             relations: {
                 courses: {
