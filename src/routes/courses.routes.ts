644974e36db8b80ca8581e92067931b041883ed8
fcorreia18 <francisco.correia1804@gmail.com>
2023-01-16 11:10:13 +0100

    feat: update course endpoint created

--- src/routes/courses.routes.ts
+++ src/routes/courses.routes.ts
@@ -3,11 +3,14 @@ import { Router } from "express";
 import { ensureAuthenticated } from "../middlewares/ensureAuthentication";
 import { CreateCourseController } from "../modules/school/useCases/createCourseUseCase/CreateCourseController";
 import { GetCourseController } from "../modules/school/useCases/getCourseUseCase/GetCourseController";
+import { UpdateCourseController } from "../modules/school/useCases/updateCourseUseCase/UpdateCourseController";
 
 const coursesRoutes = Router();
 const createCourseController = new CreateCourseController();
+const updateCourseController = new UpdateCourseController();
 const getCourseController = new GetCourseController();
 coursesRoutes.post("/", ensureAuthenticated, createCourseController.handle);
+coursesRoutes.put("/:id", ensureAuthenticated, updateCourseController.handle);
 coursesRoutes.get("/", getCourseController.handle);
 
 export { coursesRoutes };
