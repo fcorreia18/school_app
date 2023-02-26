9490cfbbad4da19e8add126256345af0aca9ef81
fcorreia18 <francisco.correia1804@gmail.com>
2023-02-26 14:05:17 +0100

    fix: getSchoolByNameUseCase return type fixed

--- src/modules/school/useCases/getSchoolByNameUseCase/GetSchoolsByNameUseCase.ts
+++ src/modules/school/useCases/getSchoolByNameUseCase/GetSchoolsByNameUseCase.ts
@@ -3,17 +3,14 @@ import { inject, injectable } from "tsyringe";
 import { School } from "../../entities/School";
 import { ISchoolRepository } from "../../repositories/ISchoolRepository";
 
-interface IResponse {
-    school: School | void;
-}
 @injectable()
 export class GetSchoolsByNameUseCase {
     constructor(
         @inject("SchoolRepository")
         private schoolRepository: ISchoolRepository
     ) {}
-    async execute(name: string): Promise<IResponse> {
-        const school: IResponse = await this.schoolRepository.findByName(name);
+    async execute(name: string): Promise<School | void> {
+        const school = await this.schoolRepository.findByName(name);
         return school;
     }
 }
